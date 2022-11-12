dbb85286e8d6f0ca771f67732b7d21ceac9331d8
Luis Dorr <Luisdorr@outlook.com>
2022-11-12 16:09:30 -0300

    a

--- Trees/src/application/Program.java
+++ Trees/src/application/Program.java
@@ -1,16 +1,37 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+
+import model.services.FileConverter;
 import model.structures.AVLTree;
 import model.structures.Node;
+import model.structures.Person;
 
 public class Program {
 
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args) throws ParseException {
+        String filePath = "C:\\Users\\Luis Dorr\\OneDrive\\Área de Trabalho";
         
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } 
+
+
 
         /* 
+        List<Person> people = FileConverter.toPeopleList(filePath);
+        System.out.println(people.toString());
+
         AVLTree<Integer> avore = new AVLTree<>(50);
         avore.add(55);
         
